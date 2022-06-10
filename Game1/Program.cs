db49b533055d5eb879344511e2168fa97dbb2c65
Gabriel3002-maker <75583405+Gabriel3002-maker@users.noreply.github.com>
2022-06-10 12:52:49 -0500

    Add files via upload

---
+++ Game1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Game1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido");
+            Random r = new Random();
+            int NumeroGanador = r.Next(1, 10);
+            bool win = false;
+
+            do
+            {
+                Console.Write("Ingrese un Valor en 0 a 10 ");
+                string s = Console.ReadLine();
+                int i = int.Parse(s);
+
+                if( i> NumeroGanador)
+                {
+                    Console.WriteLine(" El numero es alto ");
+                }
+                else if( i< NumeroGanador)
+                {
+                    Console.WriteLine(" EL numero es bajo ");
+                }
+                else if( i == NumeroGanador)
+                {
+                    Console.WriteLine(" FELICIDADES HAS GANADO =)");
+                    win = true;
+                }
+
+            } while (win == false);
+            Console.WriteLine(" Gracias por jugar !!!!");
+            Console.WriteLine(" Presione una tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,4 @@
+Es un pequeño juego de adivinar un numero entre el rango de 1 al 0
+
+Esta escrito en el lenguaje de programacion C# utilizando Visual Studio Comunity 2019
+
